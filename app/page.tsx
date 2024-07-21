16921ed39e1cdf2bec9a9ef30cabee7f16225d50
Sebastian <sebastian.danielsson@proton.me>
2024-07-21 14:38:22 +0200

    Load data client side, add loading spinner

--- app/page.tsx
+++ app/page.tsx
@@ -4,35 +4,70 @@ import React, { useState, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import { Feature, FeatureCollection } from 'geojson';
 import { ShapeJson, Valdistrikt, Rostfordelning, RosterPaverkaMandat, PartiRoster, ListRoster, Personrost, RosterOvrigaPartier, RosterEjPaverkaMandat, VotingDistrictProperties } from './electionDataInterfaces';
-import ElectionData from '../public/data/election-results/EU-val_2024_slutlig_rostfordelning_00_E.json';
 
-const electionData = ElectionData as ShapeJson;
-
-const getDistrictResults = (districtId: string | null): PartiRoster[] | null => {
+const getDistrictResults = (electionData: ShapeJson, districtId: string | null): PartiRoster[] | null => {
   if (!districtId) return null;
+
   const districtData = electionData.valdistrikt.find((d) => d.valdistriktskod === districtId);
   if (!districtData) return null;
   return districtData.rostfordelning.rosterPaverkaMandat.partiRoster;
 };
 
 const loadGeoJSONFiles = async (): Promise<FeatureCollection[]> => {
-  const context = require.context('../public/data/districts', false, /^\.\/VD_\d+_20240313_EU-val_2024\.json$/);
+  const fileUrls = [
+    'VD_01_20240313_EU-val_2024.json',
+    'VD_03_20240313_EU-val_2024.json',
+    'VD_04_20240313_EU-val_2024.json',
+    'VD_05_20240313_EU-val_2024.json',
+    'VD_06_20240313_EU-val_2024.json',
+    'VD_07_20240313_EU-val_2024.json',
+    'VD_08_20240313_EU-val_2024.json',
+    'VD_09_20240313_EU-val_2024.json',
+    'VD_10_20240313_EU-val_2024.json',
+    'VD_12_20240313_EU-val_2024.json',
+    'VD_13_20240313_EU-val_2024.json',
+    'VD_14_20240313_EU-val_2024.json',
+    'VD_17_20240313_EU-val_2024.json',
+    'VD_18_20240313_EU-val_2024.json',
+    'VD_19_20240313_EU-val_2024.json',
+    'VD_20_20240313_EU-val_2024.json',
+    'VD_21_20240313_EU-val_2024.json',
+    'VD_22_20240313_EU-val_2024.json',
+    'VD_23_20240313_EU-val_2024.json',
+    'VD_24_20240313_EU-val_2024.json',
+    'VD_25_20240313_EU-val_2024.json',
+  ];
+
   const featureCollections: FeatureCollection[] = [];
 
-  for (const key of context.keys()) {
-    const data = await context(key);
-    const transformedData: FeatureCollection = {
-      type: 'FeatureCollection',
-      features: data.features.map((feature: any): Feature => ({
-        type: 'Feature',
-        geometry: feature.geometry,
-        properties: feature.properties,
-      })),
-    };
-    featureCollections.push(transformedData);
+  for (const url of fileUrls) {
+    try {
+      const response = await fetch(`/data/districts/EPSG4326/${url}`);
+      if (!response.ok) throw new Error('Network response was not ok');
+      const data = await response.json();
+
+      const transformedData: FeatureCollection = {
+        type: 'FeatureCollection',
+        features: data.features.map((feature: any): Feature => ({
+          type: 'Feature',
+          geometry: feature.geometry,
+          properties: feature.properties,
+        })),
+      };
+
+      featureCollections.push(transformedData);
+    } catch (error) {
+      console.error('Error fetching GeoJSON data:', error);
+    }
   }
 
   return featureCollections;
+};
+
+const fetchElectionData = async (): Promise<ShapeJson> => {
+  const response = await fetch('/data/election-results/EU-val_2024_slutlig_rostfordelning_00_E.json');
+  if (!response.ok) throw new Error('Network response was not ok');
+  return response.json();
 };
 
 const closeSidebar = () => {
@@ -46,130 +81,136 @@ export default function Home() {
   const [selectedDistrict, setSelectedDistrict] = useState<null | VotingDistrictProperties>(null);
   const [districtResults, setDistrictResults] = useState<null | PartiRoster[]>(null);
   const [map, setMap] = useState<mapboxgl.Map | null>(null);
+  const [electionData, setElectionData] = useState<ShapeJson | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
-    const map = new mapboxgl.Map({
+    const newMap = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11', // Ensure you have a proper map style here
+      style: 'mapbox://styles/mapbox/standard',
       center: [16.325556, 62.3875],
       zoom: 5,
     });
 
-    map.on('load', async () => {
-      map.resize();
-      const featureCollections = await loadGeoJSONFiles();
-      console.log('Data loaded');
-
-      featureCollections.forEach((transformedData, index) => {
-        const sourceId = `voting-districts-${index}`;
-        map.addSource(sourceId, {
-          type: 'geojson',
-          data: transformedData,
-        });
-
-        map.addLayer({
-          id: `${sourceId}-fill`,
-          type: 'fill',
-          source: sourceId,
-          layout: {},
-          paint: {
-            'fill-color': '#006AA7',
-            'fill-opacity': 0.5,
-          },
-        });
-
-        map.addLayer({
-          id: `${sourceId}-outline`,
-          type: 'line',
-          source: sourceId,
-          layout: {},
-          paint: {
-            'line-color': '#000000',
-            'line-width': 0.5,
-          },
-        });
-      });
-
-      // Highlight layer, add at the end to ensure it is on top of all other layers
-      map.addSource('highlight-feature', {
-        type: 'geojson',
-        data: {
-          type: 'FeatureCollection',
-          features: [],
-        }
-      });
-
-      map.addLayer({
-        id: 'voting-districts-highlight',
-        type: 'fill',
-        source: 'highlight-feature',
-        layout: {},
-        paint: {
-          'fill-color': '#FECC02',
-          'fill-opacity': 0.5,
-        },
-      });
-
-      // Add click event handler
-      map.on('click', (e) => {
-        featureCollections.forEach((_, index) => {
-          const sourceId = `voting-districts-${index}-fill`;
-          const features = map.queryRenderedFeatures(e.point, {
-            layers: [sourceId],
-          });
-
-          if (features.length) {
-            const feature = features[0];
-            setSelectedDistrict(feature.properties as VotingDistrictProperties);
-
-            // Fetch and set district election results
-            if (feature.properties) {
-              const results = getDistrictResults(feature.properties.Lkfv);
-              setDistrictResults(results);
-            } else {
-              console.error('No properties found for the selected district');
-            }
-
-            // Update the highlight layer with the selected feature
-            const highlightSource = map.getSource('highlight-feature') as mapboxgl.GeoJSONSource;
-            highlightSource.setData({
-              type: 'FeatureCollection',
-              features: [feature],
-            });
-
-            // Show the sidebar if it's hidden
-            const sidebar = document.getElementById('sidebar');
-            if (sidebar && sidebar.classList.contains('translate-x-full')) {
-              sidebar.classList.remove('translate-x-full');
-            }
-          }
-        });
-      });
-
-      // Change the cursor to a pointer when the mouse is over the voting districts.
-      map.on('mouseenter', () => {
-        map.getCanvas().style.cursor = 'pointer';
-      });
-
-      // Change it back to default when it leaves.
-      map.on('mouseleave', () => {
-        map.getCanvas().style.cursor = '';
-      });
-    });
-
-    map.on('error', (e) => {
-      console.error('Map error:', e);
-    });
-
-    setMap(map);
+    setMap(newMap);
 
     return () => {
-      if (map) {
-        map.remove();
+      if (newMap) {
+        newMap.remove();
       }
     };
   }, []);
+
+  useEffect(() => {
+    const loadDataAndSetUpMap = async () => {
+      if (map) {
+        map.on('load', async () => {
+          map.resize();
+          const featureCollections = await loadGeoJSONFiles();
+
+          for (const [index, transformedData] of featureCollections.entries()) {
+            const sourceId = `voting-districts-${index}`;
+            map.addSource(sourceId, {
+              type: 'geojson',
+              data: transformedData,
+            });
+
+            map.addLayer({
+              id: `${sourceId}-fill`,
+              type: 'fill',
+              source: sourceId,
+              layout: {},
+              paint: {
+                'fill-color': '#006AA7',
+                'fill-opacity': 0.5,
+              },
+            });
+
+            map.addLayer({
+              id: `${sourceId}-outline`,
+              type: 'line',
+              source: sourceId,
+              layout: {},
+              paint: {
+                'line-color': '#000000',
+                'line-width': 0.5,
+              },
+            });
+          }
+
+          map.addSource('highlight-feature', {
+            type: 'geojson',
+            data: {
+              type: 'FeatureCollection',
+              features: [],
+            },
+          });
+
+          map.addLayer({
+            id: 'voting-districts-highlight',
+            type: 'fill',
+            source: 'highlight-feature',
+            layout: {},
+            paint: {
+              'fill-color': '#FECC02',
+              'fill-opacity': 0.5,
+            },
+          });
+
+          const fetchedElectionData = await fetchElectionData();
+          setElectionData(fetchedElectionData);
+          setLoading(false);
+
+          map.on('click', async (e) => {
+            for (const [index] of featureCollections.entries()) {
+              const sourceId = `voting-districts-${index}-fill`;
+              const features = map.queryRenderedFeatures(e.point, {
+                layers: [sourceId],
+              });
+
+              if (features.length) {
+                const feature = features[0];
+                setSelectedDistrict(feature.properties as VotingDistrictProperties);
+
+                if (feature.properties && fetchedElectionData) {
+                  const results = getDistrictResults(fetchedElectionData, feature.properties.Lkfv);
+                  setDistrictResults(results);
+                } else {
+                  console.error('No properties found for the selected district');
+                }
+
+                const highlightSource = map.getSource('highlight-feature') as mapboxgl.GeoJSONSource;
+                highlightSource.setData({
+                  type: 'FeatureCollection',
+                  features: [feature],
+                });
+
+                const sidebar = document.getElementById('sidebar');
+                if (sidebar && sidebar.classList.contains('translate-x-full')) {
+                  sidebar.classList.remove('translate-x-full');
+                }
+              }
+            }
+          });
+
+          map.on('mouseenter', () => {
+            map.getCanvas().style.cursor = 'pointer';
+          });
+
+          map.on('mouseleave', () => {
+            map.getCanvas().style.cursor = '';
+          });
+        });
+
+        map.on('error', (e) => {
+          console.error('Map error:', e);
+        });
+      }
+    };
+
+    loadDataAndSetUpMap();
+  }, [map]);
 
   const renderDistrictResults = (results: PartiRoster[] | null) => {
     if (!results) return null;
@@ -209,6 +250,17 @@ export default function Home() {
   return (
     <main className="relative h-dvh grid grid-cols-1 p-6">
       <div className="relative flex w-full h-full overflow-hidden">
+        {loading && (
+          <div className="absolute w-full h-full z-50 bg-gray-800/50 backdrop-blur-md flex items-center justify-center rounded-xl">
+            <div
+              className="absolute h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status">
+              <span
+                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+              >Loading...</span>
+            </div>
+          </div>
+        )}
         <div id="map" className="flex-grow rounded-xl bg-gray-100"></div>
         <div id="sidebar"
           className="overflow-scroll absolute top-0 right-0 h-full w-3/12 max-w-sm bg-gray-800/50 transform translate-x-full transition-transform duration-300 ease-in-out z-50 p-4 rounded-r-xl backdrop-blur-md text-white">
@@ -220,7 +272,6 @@ export default function Home() {
                   <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6z" /></svg>
                 </button>
               </div>
-              {/* Display results */}
               {renderDistrictResults(districtResults)}
             </div>
           ) : (
