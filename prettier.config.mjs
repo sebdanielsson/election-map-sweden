82a7661cd78c472af7d074758c41fee1f7898bd7
Sebastian <sebastian.danielsson@proton.me>
2024-09-29 00:21:13 +0200

    Bump deps, add ci, formatting, stylelint

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+const prettierConfig = {
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  printWidth: 100,
+};
+
+export default prettierConfig;
