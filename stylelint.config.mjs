1309b4431760e31da68b9cca7a88b65b8c56e206
Sebastian Danielsson <sebastian.danielsson@proton.me>
2026-01-17 00:38:35 +0100

    Bump deps, stylelint fixes

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('stylelint').Config} */
 const config = {
-  extends: ['stylelint-config-standard', 'stylelint-config-html/html'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-html/html',
+    '@dreamsicle.io/stylelint-config-tailwindcss',
+  ],
   rules: {
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['tailwind'],
-      },
-    ],
     'color-hex-length': null,
   },
 };
