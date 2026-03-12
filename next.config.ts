fb65949ab83eed5e126073f04e9075a48fe940f5
Sebastian Danielsson <sebastian.danielsson@proton.me>
2026-03-12 10:47:06 +0100

    Switch to tailwind webpack plugin
    Signed-off-by: Sebastian Danielsson <sebastian.danielsson@proton.me>

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  turbopack: {
+    rules: {
+      '*.css': {
+        loaders: ['@tailwindcss/webpack'],
+      },
+    },
+  },
 };
 
 export default nextConfig;
